636a1527a6c6eb45ba4989015ecad7f869fe4b47
Vladimir <vssmirnov@gmail.com>
2019-08-25 19:30:42 -0600

    Modify friend model

--- src/main/java/anvil/domain/model/entity/UserInfo.java
+++ src/main/java/anvil/domain/model/entity/UserInfo.java
@@ -1,6 +1,7 @@
 package anvil.domain.model.entity;
 
 import anvil.security.entities.user.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,18 +18,27 @@ import java.io.Serializable;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Table(name = "friends")
-public class Friend implements Serializable {
+public class UserInfo implements Serializable {
 
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
 
-    @ManyToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private UserInfo user;
+    @JsonIgnore
+    private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "friend_id")
-    private UserInfo friend;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username")
+    public String getUsername() {
+        return user.getUsername();
+    }
 }
